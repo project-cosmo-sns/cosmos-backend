4601d747ee294b0f2c88d0aa1dd9512a82d29cc3
Kontae <koet0928@gmail.com>
2024-04-16 22:18:20 +0900

    Chore: CORS, ValidationPipe, Hot Reload 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+declare const module: any;
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); 
+  app.enableCors({
+    origin: ['http://localhost', 'http://127.0.0.1'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // decorator(@)가 없는 속성이 들어오면 해당 속성은 제거하고 받아들입니다.
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 값이 넘어오면 request 자체를 막습니다.
+      transform: true, // request와 일치하는 class로 변환, 이 처리를 안하면 그냥 object로 넘어옴
+    }),
+  );
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
   await app.listen(8080);
 }
 bootstrap();
