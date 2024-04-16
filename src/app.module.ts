a17a0bf5817fc0c787e79cc27726f4be18a5efde
Kontae <koet0928@gmail.com>
2024-04-16 21:51:36 +0900

    Chore: TypeORM 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeORMConfigService } from './database/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeORMConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.config.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class TypeORMConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) { }
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      autoLoadEntities: true,
+      type: 'mysql',
+      host: this.configService.get<string>('DB_HOST'),
+      port: +(this.configService.get<number>('DB_PORT') || 3306),
+      username: this.configService.get<string>('DB_USER'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_NAME'),
+      entities: ['dist/**/*.entity.js'],
+      migrations: ['src/database/migrations/*.ts'],
+      synchronize: false,
+      logging: true,
+    };
+  }
+}
