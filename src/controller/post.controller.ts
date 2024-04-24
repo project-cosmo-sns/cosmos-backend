1a12e9972448cd53d32d1928837eea92f706f22d
Kontae <koet0928@gmail.com>
2024-04-25 03:42:57 +0900

    Feat: post list 정보 불러오는 api controller 생성

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PaginationResponse } from 'src/common/pagination/pagination-response';
 import { Roles } from 'src/common/roles/roles.decorator';
 import { CreatePostInfoDto } from 'src/dto/create-post-dto';
+import { SortPostList } from 'src/dto/request/sort-post-list.request';
+import { PostListResponse } from 'src/dto/response/post-list.response';
 import { RolesGuard } from 'src/guard/roles.guard';
 import { PostService } from 'src/service/post.service';
 
@@ -17,4 +20,18 @@ export class PostController {
     return this.postService.createPost(req.user.id, createPostInfo);
   }
 
+  @Roles('anyone')
+  @ApiOperation({ summary: '포스트 목록' })
+  @Get('list')
+  async getPostList(
+    @Req() req,
+    @Query() sortPostList: SortPostList
+  ): Promise<PaginationResponse<PostListResponse>> {
+    const { postInfo, totalCount } = await this.postService.getPostList(2, sortPostList);
+    return PaginationResponse.of({
+      data: PostListResponse.from(postInfo),
+      options: sortPostList,
+      totalCount,
+    })
+  }
 }
