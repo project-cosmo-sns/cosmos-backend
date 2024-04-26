1f3a057a6440f75e697d83f9b14275af7f0d9042
jyh0521 <jeonyh0521@gmail.com>
2024-04-27 00:29:16 +0900

    Feat: 세션 쿠키에 secure 빼고 httpOnly 넣음

--- src/main.ts
+++ src/main.ts
@@ -47,8 +47,7 @@ async function bootstrap() {
         maxAge: 1000 * 60 * 60 * 24,
         domain: configService.get('SESSION_COOKIE_DOMAIN'),
         path: '/',
-        httpOnly: false,
-        secure: true,
+        httpOnly: true,
       },
     }),
   );
