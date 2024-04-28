91bd8d2ba299edb4c953590a500e99ae5bcebdcd
kontae <koet0928@gmail.com>
2024-04-28 16:38:58 +0900

    Feat: 포스트 댓글 수정 api 추가

--- src/controller/post-comment.controller.ts
+++ src/controller/post-comment.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, Query, Req, UseGuards } from '@nestjs/common';
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, GoneException, InternalServerErrorException, Param, ParseIntPipe, Patch, Post, Query, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { ApiGoneResponse, ApiOperation, ApiParam, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { PaginationRequest } from 'src/common/pagination/pagination-request';
 import { PaginationResponse } from 'src/common/pagination/pagination-response';
 import { ApiPaginatedResponse } from 'src/common/pagination/pagination.decorator';
@@ -41,5 +41,28 @@ export class PostCommentController {
     @Body('content') content: string,
   ): Promise<void> {
     return this.postCommentService.writePostComment(postId, req.user.id, content);
+  }
+
+  @ApiOperation({ summary: '포스트 댓글 수정' })
+  @ApiParam({ name: 'postId', required: true, description: '포스트 id' })
+  @ApiParam({ name: 'commentId', required: true, description: '포스트 댓글 id' })
+  @ApiUnauthorizedResponse({ status: 401, description: '해당 댓글을 작성한 사람이 아닐 경우' })
+  @ApiGoneResponse({ status: 410, description: '포스트가 삭제되었거나, 댓글이 삭제된 경우' })
+  @Patch(':postId/:commentId/modify')
+  async modifyPostComment(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Param('commentId', ParseIntPipe) commentId: number,
+    @Req() req,
+    @Body('content') content: string,
+  ): Promise<void> {
+    try { return this.postCommentService.patchPostComment(postId, commentId, req.user.id, content); }
+    catch (error) {
+      if (error instanceof UnauthorizedException || error instanceof GoneException) {
+        throw error;
+      } else {
+        throw new InternalServerErrorException('서버 오류가 발생했습니다.');
+      }
+    }
+
   }
 }
