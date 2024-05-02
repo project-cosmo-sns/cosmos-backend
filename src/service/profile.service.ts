3a98f5ba630e9c14528146ec3c7e973a1a725cf0
kontae <koet0928@gmail.com>
2024-05-02 13:39:51 +0900

    Fix: 삭제된 멤버 불러오는 방식 변경

--- src/service/profile.service.ts
+++ src/service/profile.service.ts
@@ -2,6 +2,7 @@ import { GoneException, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { GetOthersProfileDto } from "src/dto/get-others-profile";
 import { Member } from "src/entity/member.entity";
+import { MemberQueryRepository } from "src/repository/member.query-repository";
 import { ProfileQueryRepository } from "src/repository/profile.query-repository";
 import { Repository } from "typeorm";
 
@@ -9,12 +10,13 @@ import { Repository } from "typeorm";
 export class ProfileService {
   constructor(
     @InjectRepository(Member) private readonly memberRepository: Repository<Member>,
+    private readonly memberQueryRepository: MemberQueryRepository,
     private readonly profileQueryRepository: ProfileQueryRepository
   ) { }
 
   async getOthersProfileInfo(memberId: number, myMemberId: number): Promise<GetOthersProfileDto> {
-    const isDeletedUser = await this.memberRepository.findOneBy({ id: memberId });
-    if (isDeletedUser?.deletedAt !== null) {
+    const isDeletedUser = await this.memberQueryRepository.getMemberIsNotDeletedById(memberId);
+    if (!isDeletedUser) {
       throw new GoneException('탈퇴한 유저입니다.');
     }
     const othersProfileInfo = await this.profileQueryRepository.getOthersProfileInfo(memberId, myMemberId);
