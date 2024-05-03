d06bb6552cf6c0cb40ee447efb797bd9e08bf437
Kontae <koet0928@gmail.com>
2024-05-04 02:21:25 +0900

    Feat: 포스트 해시태그 검색 api color도 추가

--- src/dto/response/hash-tag-search.response.ts
+++ src/dto/response/hash-tag-search.response.ts
@@ -2,15 +2,21 @@ import { ApiProperty } from "@nestjs/swagger";
 import { GetHashTagSearch } from "../get-hash-tag-search.dto";
 
 export class HashTagSearchResponse {
-  @ApiProperty({ isArray: true })
+  @ApiProperty()
   tagName!: string;
-  constructor(tagName: string) { this.tagName = tagName; }
+  @ApiProperty()
+  color!: string;
+  constructor(tagName: string, color: string) {
+    this.tagName = tagName;
+    this.color = color;
+  }
 
   static from(getHashTagSearch: GetHashTagSearch[]) {
     return getHashTagSearch.map(
       (hashTags) =>
         new HashTagSearchResponse(
-          hashTags.tagName
+          hashTags.tagName,
+          hashTags.color,
         )
     );
   }
