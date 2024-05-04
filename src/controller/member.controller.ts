8b1fa14f0555dea64225fd242e549171db75e94a
jyh0521 <jeonyh0521@gmail.com>
2024-05-04 20:06:01 +0900

    Feat: my profile image url api 타입 추가

--- src/controller/member.controller.ts
+++ src/controller/member.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GetMyProfileImageUrlResponse } from 'src/dto/response/get-my-profile-image-url.response';
+import { RolesGuard } from 'src/guard/roles.guard';
 
+@ApiTags('멤버')
 @Controller('member')
+@UseGuards(RolesGuard)
 export class MemberController {
   constructor() {}
 
+  @ApiOperation({ summary: '본인 프로필 이미지' })
+  @ApiResponse({ type: GetMyProfileImageUrlResponse })
   @Get('/profile-image-url')
   async getMyProfileImageUrl(@Req() req): Promise<GetMyProfileImageUrlResponse> {
     return GetMyProfileImageUrlResponse.from(req.user.profileImageUrl);
