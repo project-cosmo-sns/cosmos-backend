4408f5df996037c82573e1d5b5ae41d6d858add0
jyh0521 <jeonyh0521@gmail.com>
2024-05-05 20:21:55 +0900

    Feat: 포스트 작성하고 응답으로 postId => id로 내려가게 수정

--- src/dto/response/create-post.response.ts
+++ src/dto/response/create-post.response.ts
@@ -2,8 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePostResponse {
   @ApiProperty()
-  postId!: number;
-  constructor(postId: number) {
-    this.postId = postId;
+  id!: number;
+  constructor(id: number) {
+    this.id = id;
   }
 }
