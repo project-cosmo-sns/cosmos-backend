bf10cab67ebdec8f66d734d7569a478cc3b57985
Kontae <koet0928@gmail.com>
2024-05-09 23:42:33 +0900

    Feat: 인증 보내는 api 도메인 서비스로 묶고, 이미 있으면 에러 반환

--- src/service/authorization.service.ts
+++ src/service/authorization.service.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { MemberDomainService } from 'src/domain-service/member.domain-service';
 import { AuthorizationRequest } from "src/dto/request/authorization.request";
 import { Authorization } from "src/entity/authorization.entity";
 import { Member } from "src/entity/member.entity";
@@ -8,17 +9,19 @@ import { Repository } from "typeorm";
 @Injectable()
 export class AuthorizationService {
   constructor(
-    @InjectRepository(Member) private readonly memberRepository: Repository<Member>,
-    @InjectRepository(Authorization) private readonly authorizationRepository: Repository<Authorization>
+    @InjectRepository(Authorization) private readonly authorizationRepository: Repository<Authorization>,
+    private readonly memberDomainService: MemberDomainService,
   ) { }
 
   async postAuthorizationInfo(memberId: number, request: AuthorizationRequest): Promise<void> {
-    const memberInfo = await this.memberRepository.findOneBy({ id: memberId });
-    if (!memberInfo) {
-      throw new NotFoundException('해당 유저를 찾을 수 없습니다.');
-    }
+    const memberInfo = await this.memberDomainService.getMemberIsNotDeletedById(memberId);
     if (memberInfo.isAuthorized === true) {
       throw new BadRequestException('이미 승인된 유저입니다.');
+    }
+
+    const authorizationInfo = await this.authorizationRepository.findOneBy({ memberId });
+    if (authorizationInfo) {
+      throw new BadRequestException('인증 대기중입니다.');
     }
 
     await this.authorizationRepository.save({
