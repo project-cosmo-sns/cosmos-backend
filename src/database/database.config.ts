ec899538f94de9255716b539036f8d7afb3fe9a0
kontae <koet0928@gmail.com>
2024-05-09 17:22:10 +0900

    Chore: TypeORM 시간대 재설정

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -19,7 +19,7 @@ export class TypeORMConfigService implements TypeOrmOptionsFactory {
       migrations: ['src/database/migrations/*.ts'],
       synchronize: false,
       logging: true,
-      timezone: '+09:00',
+      timezone: 'Z',
     };
   }
 }
