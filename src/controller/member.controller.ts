4e8d2568252deba4a1e80825f7bdeaefdd734ec4
Kontae <koet0928@gmail.com>
2024-05-10 13:43:43 +0900

    Feat: 로그아웃 api 추가

--- src/controller/member.controller.ts
+++ src/controller/member.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GetMyProfileImageUrlResponse } from 'src/dto/response/get-my-profile-image-url.response';
 import { RolesGuard } from 'src/guard/roles.guard';
@@ -7,12 +8,19 @@ import { RolesGuard } from 'src/guard/roles.guard';
 @Controller('member')
 @UseGuards(RolesGuard)
 export class MemberController {
-  constructor() {}
+  constructor(private readonly configService: ConfigService) { }
 
   @ApiOperation({ summary: '본인 프로필 이미지' })
   @ApiResponse({ type: GetMyProfileImageUrlResponse })
   @Get('/profile-image-url')
   async getMyProfileImageUrl(@Req() req): Promise<GetMyProfileImageUrlResponse> {
     return GetMyProfileImageUrlResponse.from(req.user.profileImageUrl);
+  }
+
+  @ApiOperation({ summary: '유저 로그아웃' })
+  @Post('/logout')
+  async handleRedirect(@Res() res) {
+    res.clearCookie(this.configService.get('COOKIE_NAME'), { domain: this.configService.get('SESSION_COOKIE_DOMAIN') });
+    res.status(200).send();
   }
 }
